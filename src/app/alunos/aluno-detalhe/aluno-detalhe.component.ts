4b10ff777c16ece70bba14e8d0631892f0e1143a
heliohachimine <helio_hachimine_jr@hotmail.com>
2018-08-13 13:56:14 -0300

    telas das rotas filhas

--- src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-detalhe',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunoDetalheComponent implements OnInit {
 
-  constructor() { }
+  aluno: any;
+  inscricao: Subscription
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private alunosService: AlunosService
+  ) { }
 
   ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunosService.getAluno(id);
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
+  editarContato(){
+    this.router.navigate(['/alunos', this.aluno.id,'editar']);
   }
 
 }
--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component'
 import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service';
 
 
 
 @NgModule({
-    imports:[CommonModule, AlunosRoutingModule],
+    imports:[CommonModule, AlunosRoutingModule, FormsModule],
     exports:[],
     declarations:[AlunosComponent, AlunoFormComponent, AlunoDetalheComponent],
-    providers:[],
+    providers:[AlunosService],
 })
 
 export class AlunosModule {}
